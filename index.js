4f782d3e5079a527baf82dd8e3ca56ebc84f3d9c
Campbell Moroney <campbellmoroney@gmail.com>
2022-08-09 10:33:18 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 10;
+
+const moveBackground = (event) => {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+};
+
+const toggleContrast = () => {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+};
+
+const contact = (event) => {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_ldmuoi7",
+      "template_mcj1cuw",
+      event.target,
+      "user_D7zZDrpPytnlx5r8w7nSQ"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on campbellmoroney@gmail.com"
+      );
+    });
+};
+
+const toggleModal = () => {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+};
